21e38afc75fee02f1722d3483c17934e53c71c8d
Décio Ribeiro Filho <ribeiro.decinho@gmail.com>
2020-06-15 11:30:15 -0300

    BossSound

---
+++ AdventureSKIlls/Assets/OnBossTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBossTrigger : MonoBehaviour
+{
+    AudioSource audios;
+    public AudioClip lastmusic;
+
+    private void Awake()
+    {
+        audios = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            audios.clip = lastmusic;
+            audios.PlayOneShot(audios.clip);
+        }
+    }
+}
